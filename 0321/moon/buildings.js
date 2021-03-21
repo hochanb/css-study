1adcbfbd2a0ab08029ef9a1dc43e9439e0ce2f58
Guest <guest-ebslap@h24497.sgb.skir.kr>
2021-03-21 15:59:07 +0900

    0321-3

--- 0321/moon/buildings.js
+++ 0321/moon/buildings.js
@@ -1,16 +1,16 @@
 
-createBuildings(document.getElementById('skyline1'), 60, 5);
-//createBuildings(document.getElementById('skyline2'),60,4);
-//createBuildings(document.getElementById('skyline3'),40,3);
+createBuildings(document.getElementById('skyline1'), 40, 5);
+createBuildings(document.getElementById('skyline2'),20,4);
+createBuildings(document.getElementById('skyline3'),10,3);
 
 function createBuildings(skyline, size, z_index) {
     let w = window.innerWidth;
-    
+
     while (w > size * 2) {
-        let step=parseInt(size/10);
-        let ranW = Math.floor((0.5 + Math.random()))*step;
-        let ranH =Math.floor((0.5 + Math.random() * 1.5))*step;
-        let ranOffset = Math.floor((Math.random() - 0.5))*step;
+        let step = 8;
+        let ranW = Math.floor((0.5 + Math.random()) * 0.8 * size / step) * step;
+        let ranH = Math.floor((0.8 + Math.random() * 2.5) * size / step) * step;
+        let ranOffset = Math.floor((Math.random() - 0.5) * size / step) * step;
         let building = document.createElement('span');
         building.setAttribute("class", "building");
         building.style.width = ranW + 'px';
@@ -21,8 +21,21 @@ function createBuildings(skyline, size, z_index) {
 
         w -= (ranW + ranOffset);
     }
-    skyline.querySelector('.building').style.marginRight=w/2;
+
+    /* 마지막 채워주기 */
+    let step = 8;
+    let ranW = Math.floor(w/step) * step;
+    let ranH = Math.floor((0.8 + Math.random() * 2.5) * size / step) * step;
+    let ranOffset = 0;
+    let building = document.createElement('span');
+    building.setAttribute("class", "building");
+    building.style.width = ranW + 'px';
+    building.style.height = ranH + 'px';
+    building.style.marginRight = ranOffset + 'px';
+    building.style.zIndex = z_index;
+    skyline.appendChild(building);
 }
+
 function createWindows(building) {
 
 }
